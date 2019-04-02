d5a8d4e0579113eebbb6a07935391a33ead83954
丁巍巍 <daylsu@163.com>
2019-04-03 00:01:21 +0800

    init

---
+++ EZOpenDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZOpenDemo
+//
+//  Created by will on 2019/4/2.
+//  Copyright © 2019年 will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZOpenDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZOpenDemo
+//
+//  Created by will on 2019/4/2.
+//  Copyright © 2019年 will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
